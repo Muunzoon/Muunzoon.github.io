760e69c340d62a293ca08389b22dea96d48ce8ba
Muunzoon <dutonin@hotmail.com>
2016-08-22 18:04:52 -0700

    Update ejercicioarbol.js

--- rv/ejercicioarbol.js
+++ rv/ejercicioarbol.js
@@ -9,6 +9,6 @@ var forma2 = new THREE.SphereGeometry( 7, 32, 32 );
 var material = new THREE.MeshNormalMaterial();
 var cilindro = new THREE.Mesh( forma1, material );
 var esfera = new THREE.Mesh(forma2, material );
-esfera.traslateY(20);
+
 escena.add( esfera );
 renderizador.render( escena, camara );
