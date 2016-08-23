bfcff3e8aca5cf808146a2e6dd563a8830038085
Muunzoon <dutonin@hotmail.com>
2016-08-22 18:03:44 -0700

    Update ejercicioarbol.js

--- rv/ejercicioarbol.js
+++ rv/ejercicioarbol.js
@@ -5,8 +5,10 @@ var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95,window.innerHeight*.95);
 document.body.appendChild( renderizador.domElement );
 var forma1 = new THREE.CylinderGeometry( 5, 5, 20, 32 );
-
+var forma2 = new THREE.SphereGeometry( 7, 32, 32 );
 var material = new THREE.MeshNormalMaterial();
 var cilindro = new THREE.Mesh( forma1, material );
-escena.add( cilindro );
+var esfera = new THREE.Mesh(forma2, material );
+esfera.traslateY(20);
+escena.add( esfera );
 renderizador.render( escena, camara );
