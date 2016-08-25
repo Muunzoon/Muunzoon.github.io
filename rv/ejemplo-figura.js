37dde19b6c55cd0e65a36b2f10fab4617078e81f
Muunzoon <dutonin@hotmail.com>
2016-08-25 15:06:36 -0500

    Update ejemplo-figura.js

--- rv/ejemplo-figura.js
+++ rv/ejemplo-figura.js
@@ -9,12 +9,13 @@ var forma = new THREE.ShapeGeometry(figura);
 var malla = new THREE.mesh(forma);
 
 var escena = new THREE.Scene();
-escena.add(malla);
+escena.add( malla );
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 100;
+camara.position.z = 5;
 
 var renderizador = new THREE.WebGLRenderer();
-renderizador.setSize( window.innerHeight*.95, window.innerHeight*.95 );
+renderizador.setSize( window.innerHeight*.95,
+			window.innerHeight*.95 );
 document.body.appendChild( renderizador.domElement );
 renderizador.render( escena, camara );
