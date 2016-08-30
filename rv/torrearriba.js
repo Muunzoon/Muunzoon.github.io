216cb1a5075deaeee1f737f4fe016de5b9d7bce8
Muunzoon <dutonin@hotmail.com>
2016-08-30 15:48:06 -0500

    Update torrearriba.js

--- rv/torrearriba.js
+++ rv/torrearriba.js
@@ -1,12 +1,16 @@
 var troncoForma = new THREE.CylinderGeometry(2.5, 5, 10);
+var basee = new THREE.CylinderGeometry(6,6,2);
+basee.translate(0-,10,0);
 var esferaForma = new THREE.SphereGeometry(6.5);
 esferaForma.translate(0,10,0);
 
+var baseeMalla = new THREE.Mesh(basee);
 var troncoMalla = new THREE.Mesh(troncoForma);
 var esferaMalla = new THREE.Mesh(esferaForma);
 
 var arbolForma = new THREE.Geometry();
 
+arbolForma.merge(baseeMalla.geometry, baseeMalla.matrix);
 arbolForma.merge(troncoMalla.geometry, troncoMalla.matrix);
 arbolForma.merge(esferaMalla.geometry, esferaMalla.matrix);
 
