2680671639d88f855553907a6d37a0552500b9d9
Muunzoon <dutonin@hotmail.com>
2016-08-30 15:14:28 -0500

    Update torrearriba.js

--- rv/torrearriba.js
+++ rv/torrearriba.js
@@ -4,6 +4,11 @@ figura.moveTo(10, 10);
 figura.lineTo(10, 40);
 figura.lineTo(40, 40);
 figura.lineTo(10, 10);
+
+figura.moveTo(-10, -10);
+figura.lineTo(10, -40);
+figura.lineTo(-40, -40);
+figura.lineTo(-10, -10);
 var forma = new THREE.ExtrudeGeometry( figura, {amount: 10} );
 
 var material = new THREE.MeshNormalMaterial();
