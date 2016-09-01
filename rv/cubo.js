3505b22d5d8800238fdf5beab3d9add017eb5666
Muunzoon <dutonin@hotmail.com>
2016-09-01 14:53:30 -0700

    Update cubo.js

--- rv/cubo.js
+++ rv/cubo.js
@@ -10,6 +10,7 @@ forma.vertices.push( new THREE.Vector3( -1, 1, 1 ) );
 
 forma.faces.push( new THREE.Face3( 0, 1, 2 ) );
 forma.faces.push( new THREE.Face3( 0, 2, 3 ) );
+forma.faces.push( new THREE.Face3( 2, 1, 6 ) );
 
 forma.computeBoundingSphere();
 forma.computeFaceNormals();
