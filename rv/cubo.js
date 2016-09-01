6eaf80958983735b7cfc72c22ff963df1dd75f9f
Muunzoon <dutonin@hotmail.com>
2016-09-01 15:03:25 -0700

    Update cubo.js

--- rv/cubo.js
+++ rv/cubo.js
@@ -13,7 +13,8 @@ forma.faces.push( new THREE.Face3( 0, 2, 3 ) );
 forma.faces.push( new THREE.Face3( 2, 1, 6 ) );
 forma.faces.push( new THREE.Face3( 1, 5, 6 ) );
 forma.faces.push( new THREE.Face3( 6, 3, 2 ) );
-forma.faces.push( new THREE.Face3( 7, 6, 3 ) );
+forma.faces.push( new THREE.Face3( 0, 1, 5 ) );
+forma.faces.push( new THREE.Face3( 3, 7, 6 ) );
 
 
 
