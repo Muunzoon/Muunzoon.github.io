0edd60ea2c9abe504fff94161e475d7cc08779c5
Muunzoon <dutonin@hotmail.com>
2016-09-01 15:21:54 -0500

    Update torrearriba.js

--- rv/torrearriba.js
+++ rv/torrearriba.js
@@ -1,7 +1,7 @@
 var troncoForma = new THREE.CylinderGeometry(10, 20, 40);
 var basee = new THREE.CylinderGeometry(24,24,8);
 basee.translate(0,-20,0);
-var esferaForma = new THREE.SphereGeometry(26);
+var esferaForma = new THREE.BoxGeometry( 26, 26, 26);
 esferaForma.translate(0,40,0);
 var baseabajo = new THREE.CylinderGeometry(32,32,8);
 baseabajo.translate(0,-24,0);
