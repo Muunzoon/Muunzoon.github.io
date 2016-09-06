48100fc098d6699efec1eac89fe1b8be0db92f95
Muunzoon <dutonin@hotmail.com>
2016-09-06 14:54:11 -0500

    Create camaras.js

---
+++ rv/camaras.js
@@ -0,0 +1,23 @@
+var campoVision = 45;//grados
+var relacionAspecto = window.innerWidth / window.innerHeight;
+var planoCercano = 1;
+var planoLejano = 1000;
+
+var camara = new THREE.PerspectiveCamera( campoVision, relacionAspecto, planoCercano, planoLejano);
+
+camara.position.z = 15;
+
+var cubo = new THREE.Mesh( new THREE.BoxGeometry( 2, 2, 2 ), new THREE.MeshNormalMaterial() );
+
+cubo.rotateY(Math.PI/4);
+
+var esfera2 = new THREE.Mesh( new THREE.SphereGeometry( 1 ), new THREE.MeshNormalMaterial() );
+esfera2.position.x = 5;
+esfera2.position.z = -10;
+
+var escena = new THREE.Scene();
+ascena.add( esfera2 );
+escena.add( cubo );
+var renderizador = new THREE.WebGLRenderer();
+renderizador.setSize( window.innerHeight*.95,window.innerHeight*.95);
+document.body.appendChild( renderizador.domElement );
