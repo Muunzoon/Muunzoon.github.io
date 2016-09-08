e93189ea9d26544cb6f3557a686bbba67d6b15b5
Muunzoon <dutonin@hotmail.com>
2016-09-08 16:01:38 -0500

    Update tableroajedrez.js

--- rv/tableroajedrez.js
+++ rv/tableroajedrez.js
@@ -94,6 +94,7 @@ var escena = new THREE.Scene();
 //arbolMalla , Torre2 , Torre3 , Torre4 ,
 for ( var l=0; l<32; l ++)
 escena.add(  tablero [l] );
+escena.rotateX( Math.PI/4 );
 
 var camara = new THREE.PerspectiveCamera();
 camara.position.z = 300;
