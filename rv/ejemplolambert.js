dbb8b7b59dc50c3a7e075e3e98a030d9e2b93c9c
Muunzoon <dutonin@hotmail.com>
2016-09-13 15:17:25 -0500

    Update ejemplolambert.js

--- rv/ejemplolambert.js
+++ rv/ejemplolambert.js
@@ -4,7 +4,7 @@ luzPuntual.position.y = 10;
 luzPuntual.position.z = 10;
 
 var forma = new THREE.SphereGeometry( 1 );
-var material = new THREE.MeshBasicMaterial( {color: 0x00ff00 } );
+var material = new THREE.MeshLambertMaterial( {color: 0x00ff00 } );
 var malla = new THREE.Mesh( forma, material );
 
 var escena = new THREE.Scene();
@@ -14,8 +14,8 @@ escena.add(luzPuntual);
 var camara = new THREE.PerspectiveCamera();
 camara.position.z = 5;
 
-var lienzo = document.getElementById("luzPuntual");
-var renderizador = new THREE.WebGLRenderer({canvas: lienzo, antialias: true});
+
+var renderizador = new THREE.WebGLRenderer();
 
 renderizador.setSize( 600, 600 );
 document.body.appendChild( renderizador.domElement );
