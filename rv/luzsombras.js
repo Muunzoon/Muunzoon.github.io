eccc10b6080de628e8f3d7f53b2b3c28d873134d
Muunzoon <dutonin@hotmail.com>
2016-09-20 15:16:39 -0500

    Update luzsombras.js

--- rv/luzsombras.js
+++ rv/luzsombras.js
@@ -7,7 +7,7 @@ var malla = new THREE.Mesh( forma, material );
 malla.position.y = 2;
 ///////////////
 malla.transparent =  true;
-malla.opacity = .5;
+malla.opacity = .1;
 /////////////
 var base = new THREE.Mesh( new THREE.BoxGeometry(5,.1,5), new THREE.MeshLambertMaterial({color: 0xFFFFFF}));
 
