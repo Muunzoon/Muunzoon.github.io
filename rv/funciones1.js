cf9363f6ddb8649cd9d1a71d652fd5251191c1ab
Muunzoon <dutonin@hotmail.com>
2016-09-22 15:05:38 -0500

    Create funciones1.js

---
+++ rv/funciones1.js
@@ -0,0 +1,18 @@
+function init(P){
+  var malla = new THREE.Mesh(new THREE.BoxGeometry(P, P, P), new THREE.MeshNormalMaterial());
+  escena = new THREE.Scene();
+  escena.add(malla);
+  camara = new THREE.PerspectiveCamera();
+  renderizador = new THREE.WebGLRender();
+  renderizador.setSize(700, 700);
+  document.body.appendChild( renderizador.domElement );
+  renderizador.render( escena, camara );
+  
+}
+
+var main = function(P){
+  renderizador.render(escena, camara);
+  }
+  var escena, camara, renderizador;
+  init(1);
+  main(2);
