d5ecc692613ce7155e1403b745ff4e5b8338a5c8
Muunzoon <dutonin@hotmail.com>
2016-09-22 15:12:54 -0500

    Update funciones1.js

--- rv/funciones1.js
+++ rv/funciones1.js
@@ -3,6 +3,9 @@ function init(P){
   escena = new THREE.Scene();
   escena.add(malla);
   camara = new THREE.PerspectiveCamera();
+  var camara = new THREE.PerspectiveCamera();
+camara.position.z = 15;
+camara.position.y = 5;
   renderizador = new THREE.WebGLRenderer();
   renderizador.setSize(700, 700);
   document.body.appendChild( renderizador.domElement );
