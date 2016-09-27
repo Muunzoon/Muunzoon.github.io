2a42fffbc3729b849747cdca8094c52a979a4f91
Muunzoon <dutonin@hotmail.com>
2016-09-27 15:49:31 -0500

    Update ejetexturas.js

--- rv/ejetexturas.js
+++ rv/ejetexturas.js
@@ -13,9 +13,9 @@ TEXTURA.setup = function() {
   TEXTURA.camara = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
   TEXTURA.camara.position.z = 5;
   
-  var lienzo = document.getElementById("ejemplotextura");
   TEXTURA.renderizador = new THREE.WebGLRenderer({canvas: lienzo, antialias: true});
   TEXTURA.renderizador.setSize( 600, 600 );
+  domcument.body.appendChild(TEXTURA.renderizador.domElement);
 }
 
 TEXTURA.loop = function (){
