40a9b778a81d97a28197120744ea8479e4076eaa
Muunzoon <dutonin@hotmail.com>
2016-09-27 14:59:49 -0500

    Create ejevent.js

---
+++ rv/ejevent.js
@@ -0,0 +1,31 @@
+var VENTANA = new Object();
+VENTANA.listener = function() {
+  VENTANA.camara.aspect = window.innerWidth / window.innerHeight;
+  VENTANA.camara.updateProjectionMatrix();
+  VENTANA.renderizador.setSize( window.innerWhidth, window.innerHeight); 
+}
+
+VENTANA.setup = function() {
+  var tipo_evento = 'resize';
+  var capturarp = false;
+  window.addEventListener( tipo_evento, VENTANA.listener, capturarp );
+  
+  VENTANA.escena = new THREE.Scene();
+  VENTANA.camara = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, .1, 1000);
+  VENTANA.camara.position.z = 5;
+  
+  var lienzo = document.getElementById("ejemplo-ventana").
+  VENTANA.renderizador = new THREE.WebGLRenderer({canvas: lienzo, antialias: true});
+  VENTANA.renderizador.setSize( window.innerWhidth, window.innerHeight); 
+  VENTANA.malla = new THREE.Mesh( new THREE.BoxGeometry( 1,1,1), new THREE.MeshNormalMaterial());
+  VENTANA.escena.add(VENTANA.malla);
+}
+
+VENTANA.loop = function (){
+  requestAnimationFrame(  VENTANA.loop);
+  VENTANA.malla.rotateX( .01 );
+  VENTANA.malla.rotateY( .01 );
+  VENTANA.renderizador.render( VENTANA.escena. VENTANA.camara);
+}
+VENTAANA.setup();
+VENTANA.loop();
