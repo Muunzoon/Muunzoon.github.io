e2ebb4c1e2bd47661492287bd01e1ed2c7a7bdcb
Muunzoon <dutonin@hotmail.com>
2016-09-27 15:06:51 -0500

    Update ejevent.js

--- rv/ejevent.js
+++ rv/ejevent.js
@@ -2,7 +2,7 @@ var VENTANA = new Object();
 VENTANA.listener = function() {
   VENTANA.camara.aspect = window.innerWidth / window.innerHeight;
   VENTANA.camara.updateProjectionMatrix();
-  VENTANA.renderizador.setSize( window.innerWhidth, window.innerHeight); 
+  VENTANA.renderizador.setSize( window.innerWidth, window.innerHeight); 
 }
 
 VENTANA.setup = function() {
