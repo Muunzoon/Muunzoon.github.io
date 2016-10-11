a2ba4333ec81119e40395965603d0978fd3a0afc
Muunzoon <dutonin@hotmail.com>
2016-10-11 07:14:15 -0700

    Update cargafbx.js

--- rv/cargafbx.js
+++ rv/cargafbx.js
@@ -14,7 +14,7 @@ var escena = new THREE.Scene();
 escena.add( cubo );/////////////////////
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 300;
+camara.position.z = 30;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95,
