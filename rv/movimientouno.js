13d24e003adcea3ac27283d3e44f2dc6c5c3df99
Muunzoon <dutonin@hotmail.com>
2016-10-20 15:57:03 -0500

    Update movimientouno.js

--- rv/movimientouno.js
+++ rv/movimientouno.js
@@ -4,11 +4,11 @@ function Pieza(){
   this.piernaDer = new THREE.Mesh(new THREE.BoxGeometry(1,5,1));
   var cuerpo = new THREE.Mesh(new THREE.BoxGeometry(5,10,5));
   this.add(this.piernaIzq,this.piernaDer,cuerpo);
-  this.piernaIzq.position.z=-2;
+  this.piernaIzq.position.x=-2;
   this.piernaIzq.position.y=-7.5;
-  this.piernaDer.position.z=2;
-  this.piernaDer.position.z=-7.5;
-  this.piernaDer.position.x=-7.5;
+  //this.piernaDer.position.z=2;
+  this.piernaDer.position.y=-7.5;
+  this.piernaDer.position.x=2;
   cuerpo.position.z=2.5;
   }
   var pieza;
