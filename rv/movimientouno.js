328f458483b3d0f3d1fcff0aee81198fb8b427ce
Muunzoon <dutonin@hotmail.com>
2016-10-20 15:30:52 -0500

    Update movimientouno.js

--- rv/movimientouno.js
+++ rv/movimientouno.js
@@ -19,14 +19,8 @@ function Pieza(){
     var material1 = new THREE.MeshBasicMaterial( { color: 0x84422e } );
     var pata = new THREE.Mesh( pieza, material1 );
     //////////////////////////////
-    var campoVision = 45;//grados
-var relacionAspecto = window.innerWidth / window.innerHeight;
-var planoCercano = 1;
-var planoLejano = 1000;
-
-var camara = new THREE.PerspectiveCamera( campoVision, relacionAspecto, planoCercano, planoLejano);
-
-camara.position.z = 15;
+var camara = new THREE.PerspectiveCamera();
+camara.position.z = 5;
 
 var escena = new THREE.Scene();
 escena.add( pata );
