92fc1de6908b6b46ee6a7c4f069d62a9a03c87ab
Muunzoon <dutonin@hotmail.com>
2016-10-27 15:53:37 -0500

    Update ejerobot.js

--- rv/ejerobot.js
+++ rv/ejerobot.js
@@ -114,7 +114,7 @@ Enviroment.prototype.setMap=function(map){
     for (var j =0; j<map.length;j++){
       if (map[i][j]==="x")
         this.add(new Wall(1,j-_offset,-(i-_offset)));
-      else
+      else if (map[i][j]==="r")
         this.add(new Robot(0.5, j-_offset,-(i-_offset)));
     }
 }
