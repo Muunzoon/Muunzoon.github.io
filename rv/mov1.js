4f2d51300819eae356a05f357409ccd33583c7b9
Muunzoon <dutonin@hotmail.com>
2016-11-03 15:20:15 -0600

    Update mov1.js

--- rv/mov1.js
+++ rv/mov1.js
@@ -81,4 +81,54 @@ var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95,
 			window.innerHeight*.95 );
 document.body.appendChild( renderizador.domElement );
-renderizador.render( escena, camara );
+
+
+
+
+///////////////
+var control;
+render();
+control = new THREE.TransformControls( camara, renderizador.domElement );
+control.addEventListener( 'change', render );
+escena.add( control );
+
+control.attach( arbolmalla );
+scene.add( control );
+
+window.addEventListener( 'resize', onWindowResize, false );
+window.addEventListener( 'keydown', function ( event ) {}
+			switch ( event.keyCode ) {
+						case 81: // Q
+							control.setSpace( control.space === "local" ? "world" : "local" );
+							break;
+						case 17: // Ctrl
+							control.setTranslationSnap( 100 );
+							control.setRotationSnap( THREE.Math.degToRad( 15 ) );
+							break;
+						case 87: // W
+							control.setMode( "translate" );
+							break;
+						case 69: // E
+							control.setMode( "rotate" );
+							break;
+						case 82: // R
+							control.setMode( "scale" );
+							break;
+						case 187:
+						case 107: // +, =, num+
+							control.setSize( control.size + 0.1 );
+							break;
+						case 189:
+						case 109: // -, _, num-
+							control.setSize( Math.max( control.size - 0.1, 0.1 ) );
+							break;
+					}
+				});
+
+function render() {
+				control.update();
+				renderizador.render( escena, camara );
+			}
+
+//////////////
+
