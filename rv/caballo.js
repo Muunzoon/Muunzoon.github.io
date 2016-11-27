4958f798b4d7a606110ab8e60c6dfb0793041e7f
Muunzoon <dutonin@hotmail.com>
2016-11-26 18:46:20 -0800

    Update caballo.js

--- rv/caballo.js
+++ rv/caballo.js
@@ -1,9 +1,24 @@
 var figura = new THREE.Shape();
-
-figura.moveTo(10, 10);
-figura.lineTo(10, 40);
-figura.lineTo(40, 40);
-figura.lineTo(10, 10);
+float Arx[]={,13,13,14,14};
+float Ary[]={,188,188,186,186,184,184};
+8,7,7,7,9,12,14,14,16,17,15,13,12,13,14
+184,186,186,189,192,194,195,196,194,193,191,190,190,188,184        0=184
+figura.moveTo(8, 0);
+figura.lineTo(7, 2);
+figura.lineTo(7, 2);
+figura.lineTo(7, 5);
+figura.lineTo(9, 8);
+figura.lineTo(12, 10);
+figura.lineTo(14, 11);
+figura.lineTo(14, 12);
+figura.lineTo(16, 10);
+figura.lineTo(17, 9);
+figura.lineTo(15, 7);
+figura.lineTo(13, 6);
+figura.lineTo(12, 6);
+figura.lineTo(13, 4);
+figura.lineTo(14, 0);
+figura.lineTo(8, 0);
 var forma = new THREE.ExtrudeGeometry( figura, {amount: 10} );
 
 var material = new THREE.MeshNormalMaterial();
