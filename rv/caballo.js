5c7d7b154aa09e74734c0f26462770c2a466b075
Muunzoon <dutonin@hotmail.com>
2016-11-26 19:02:50 -0800

    Update caballo.js

--- rv/caballo.js
+++ rv/caballo.js
@@ -41,7 +41,7 @@ var escena = new THREE.Scene();
 escena.add( malla );
 
 var camara = new THREE.PerspectiveCamera();
-camara.position.z = 50;
+camara.position.z = 100;
 
 var renderizador = new THREE.WebGLRenderer();
 renderizador.setSize( window.innerHeight*.95,
